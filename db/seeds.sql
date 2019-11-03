5bd8e871c46737b7ec0afbb0fe84a0923396175e
siturbe82 <santiago.iturbe@gmail.com>
2019-11-02 21:58:29 -0500

    Adding the functioning files, but have yet to deploy or fully format

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Plain Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Super Jalapeno Double Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Angus Deluxe Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Longhorn Patty', true);
+INSERT INTO burgers (name) VALUES ('Monster Burger with Bacon');
