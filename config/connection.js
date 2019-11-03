4fb357d2aa6edd2abec45ac24915cf15fee11846
siturbe82 <santiago.iturbe@gmail.com>
2019-11-03 09:45:17 -0600

    adding ClearDB to connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-iron-east-05.cleardb.net",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
+  user: "b047943f2baad7",
+  password: "66f62196",
+  database: "heroku_23652f1b381b1f7"
 });
 
 // Make connection.
